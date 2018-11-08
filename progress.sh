df5bdade87a70cd9065e78cc39889badf83f756e
Sam Ireland <sam.ireland.uk@gmail.com>
2018-11-08 00:49:27 +0000

    Add progress tracker

---
+++ progress.sh
@@ -0,0 +1,28 @@
+IFS=$'\n'
+
+# Create directory to go nuts in
+
+# Create list of times
+times="$(git log | grep -E -i -w -oh 'Date: (.+?)$')"
+times=($times)
+
+# Create list of commits
+commits="$(git log | grep -E -i -w -oh '[a-f0-9]{40}$')"
+commits=($commits)
+
+counts=()
+for i in "${!commits[@]}"; do
+  `git checkout "${commits[i]}" --quiet`
+  counts+=("$(texcount Thesis.tex | grep -E -i -w -oh 'in text\: (\d+)' | cut -d ' ' -f 3)")
+done
+
+`git checkout "${commits[0]}" --quiet`
+
+for i in "${!commits[@]}"; do
+  times[i]="'${times[i]}'"
+done
+
+counts="$( IFS=$','; echo "${counts[*]}" )"
+times="$( IFS=$','; echo "${times[*]}" )"
+sed -i'.bak' -E "s/counts = \[.+\]/counts = [$counts]/" index.html
+sed -i'.bak' -E "s/times = \[.+\]/times = [$times]/" index.html
